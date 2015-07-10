bd14dd6677fb47441e5ec44396f0420e2f1b5608
Joe Wemyss <joewemyss3@gmail.com>
2015-07-10 11:56:44 +0100

    Added custom directives to the button element

--- src/angularjstutorial.js
+++ src/angularjstutorial.js
@@ -13,16 +13,34 @@ app.controller('MainCtrl', function($scope)
 app.controller("SecondCtrl", function ($scope) {
     $scope.name = "Joe";
 });
-app.directive('walterwhite', function () {
-    return {
-        /*This ensures that the directive is restricted
-        * to being an element*/
-        restrict: 'E',
-        /*This tells the directive that every time it is updated it
-        * will overwrite itself*/
-        transclude: true,
-        /*This is what will be included in the directive*/
-        template: '<h2>I Am Hiesenberg</h2>'
-
+/*The name of this directive is hyphenated in the html file
+* but angular reads it as camel case so it must be camel
+* cased here*/
+app.directive('interactiveButton', function () {
+    return{
+        /*This will restrict the directive to being an attribute*/
+        restrict : 'A',
+        link: function ($scope, $element, $attr) {
+            $element.bind('mouseenter', function () {
+                console.log($element);
+                /*Change the innerHTML*/
+                $element[0].innerHTML = "Rolled Over";
+            });
+            $element.bind('mouseleave', function () {
+                $element[0].innerHTML ="Click Me";
+            });
+        }
     }
+})
+
+.directive('walterwhite', function () {
+    return{
+        restrict: 'E',
+        transclude: true,
+        link : function ($scope, $element, $attr) {
+            /*console.log($scope);
+            console.log($element);
+            console.log($attr);*/
+        }
+    };
 });
